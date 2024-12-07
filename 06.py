ee5b1049c97f81625bafbfda7bcf037bcaa3a1e3
Algirdas Neniskis <61432976+algirdasN@users.noreply.github.com>
2024-12-07 20:37:54 +0200

    Problem 6 - part 2 slow

--- 06.py
+++ 06.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from copy import deepcopy
 from itertools import cycle
 
 DIRS = [
@@ -30,13 +32,44 @@ def main():
         if not (0 <= next_cell[0] < len(grid) and 0 <= next_cell[1] < len(grid[0])):
             break
 
-        if grid[next_cell[0]][next_cell[1]] == "#":
+        while grid[next_cell[0]][next_cell[1]] == "#":
             curr_dir = next(dir_iter)
-            curr = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
-        else:
-            curr = next_cell
+            next_cell = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
+
+        curr = next_cell
 
     print(len(visited))
+
+    visited.discard(start)
+
+    total = 0
+    for block_cell in visited:
+        grid_copy = deepcopy(grid)
+        grid_copy[block_cell[0]][block_cell[1]] = "#"
+
+        visited_dict = defaultdict(set)
+
+        curr = start
+        dir_iter = cycle(DIRS)
+        curr_dir = next(dir_iter)
+        while True:
+            if curr in visited_dict[curr_dir]:
+                total += 1
+                break
+
+            visited_dict[curr_dir].add(curr)
+            next_cell = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
+
+            if not (0 <= next_cell[0] < len(grid_copy) and 0 <= next_cell[1] < len(grid_copy[0])):
+                break
+
+            while grid_copy[next_cell[0]][next_cell[1]] == "#":
+                curr_dir = next(dir_iter)
+                next_cell = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
+
+            curr = next_cell
+
+    print(total)
 
 
 if __name__ == "__main__":
