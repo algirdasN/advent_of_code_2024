bcdb457756bef1930cfe207fc2301a1ead44a1bc
Algirdas Neniskis <61432976+algirdasN@users.noreply.github.com>
2024-12-08 19:32:24 +0200

    Problem 8 - part 2

--- 08.py
+++ 08.py
@@ -15,7 +15,9 @@ def main():
                 antennas[grid[i][j]].add((i, j))
 
     antinodes = set()
+    antinodes_harmonics = set()
     for values in antennas.values():
+        antinodes_harmonics.update(values)
         pairs = permutations(values, 2)
         for node1, node2 in pairs:
             x_diff = node1[0] - node2[0]
@@ -23,13 +25,26 @@ def main():
             antinode1 = (node1[0] + x_diff, node1[1] + y_diff)
             antinode2 = (node2[0] - x_diff, node2[1] - y_diff)
 
-            if 0 <= antinode1[0] < x_len and 0 <= antinode1[1] < y_len:
-                antinodes.add(antinode1)
+            first = True
+            while 0 <= antinode1[0] < x_len and 0 <= antinode1[1] < y_len:
+                if first:
+                    antinodes.add(antinode1)
+                    first = False
 
-            if 0 <= antinode2[0] < x_len and 0 <= antinode2[1] < y_len:
-                antinodes.add(antinode2)
+                antinodes_harmonics.add(antinode1)
+                antinode1 = (antinode1[0] + x_diff, antinode1[1] + y_diff)
+
+            first = True
+            while 0 <= antinode2[0] < x_len and 0 <= antinode2[1] < y_len:
+                if first:
+                    antinodes.add(antinode2)
+                    first = False
+
+                antinodes_harmonics.add(antinode2)
+                antinode2 = (antinode2[0] - x_diff, antinode2[1] - y_diff)
 
     print(len(antinodes))
+    print(len(antinodes_harmonics))
 
 
 if __name__ == "__main__":
