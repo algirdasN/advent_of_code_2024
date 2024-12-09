86317d5d535d8b2e31e38e2c0bea3a67f4d9c1db
Algirdas Neniskis <61432976+algirdasN@users.noreply.github.com>
2024-12-09 20:03:58 +0200

    Problem 9 - part 2

--- 09.py
+++ 09.py
@@ -34,6 +34,60 @@ def main():
 
     print(checksum)
 
+    data_list = []
+    for i in range(len(data)):
+        size = int(data[i])
+        if size == 0:
+            continue
+
+        value = i // 2 if i % 2 == 0 else -1
+        data_list.append((value, size))
+
+    r_pointer = 0
+    while True:
+        r_pointer -= 1
+        if r_pointer < -len(data_list):
+            break
+
+        if data_list[r_pointer][0] == -1:
+            continue
+
+        size = data_list[r_pointer][1]
+
+        for l_pointer in range(len(data_list) + r_pointer):
+            if data_list[l_pointer][0] != -1:
+                continue
+
+            space = data_list[l_pointer][1]
+            if size > space:
+                continue
+
+            data_list[l_pointer] = data_list[r_pointer]
+
+            if size < space:
+                data_list.insert(l_pointer + 1, (-1, space - size))
+
+            if r_pointer + 1 < 0 and data_list[r_pointer + 1][0] == -1:
+                size += data_list[r_pointer + 1][1]
+                data_list.pop(r_pointer + 1)
+                r_pointer += 1
+
+            if data_list[r_pointer - 1][0] == -1:
+                size += data_list[r_pointer - 1][1]
+                data_list.pop(r_pointer - 1)
+
+            data_list[r_pointer] = (-1, size)
+            break
+
+    bit_counter = 0
+    checksum = 0
+    for file in data_list:
+        if file[0] > 0:
+            checksum += file[0] * file[1] * (2 * bit_counter + file[1] - 1) // 2
+        bit_counter += file[1]
+
+    print(checksum)
+
 
 if __name__ == "__main__":
     main()
