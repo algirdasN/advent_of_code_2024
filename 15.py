9d85cec444f994f38baa4246d62a7989ffac4327
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2024-12-18 21:45:50 +0200

    Problem 15 - cleanup

--- 15.py
+++ 15.py
@@ -23,24 +23,7 @@ def play_regular(grid_str, moves):
 
     for move in moves:
         curr_dir = DIRS[move]
-        next_cell = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
-
-        if grid[next_cell[0]][next_cell[1]] == EMPTY:
-            curr = next_cell
-            continue
-
-        if grid[next_cell[0]][next_cell[1]] == WALL:
-            continue
-
-        front = next_cell
-
-        while grid[front[0]][front[1]] == BOX:
-            front = (front[0] + curr_dir[0], front[1] + curr_dir[1])
-
-        if grid[front[0]][front[1]] == EMPTY:
-            grid[next_cell[0]][next_cell[1]] = EMPTY
-            grid[front[0]][front[1]] = BOX
-            curr = next_cell
+        curr = move_regular(grid, curr, curr_dir)
 
     total = 0
     for i in range(len(grid)):
@@ -49,6 +32,28 @@ def play_regular(grid_str, moves):
                 total += 100 * i + j
 
     return total
+
+
+def move_regular(grid, curr, curr_dir):
+    next_cell = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
+
+    if grid[next_cell[0]][next_cell[1]] == EMPTY:
+        return next_cell
+
+    if grid[next_cell[0]][next_cell[1]] == WALL:
+        return curr
+
+    front = next_cell
+
+    while grid[front[0]][front[1]] == BOX:
+        front = (front[0] + curr_dir[0], front[1] + curr_dir[1])
+
+    if grid[front[0]][front[1]] != EMPTY:
+        return curr
+
+    grid[next_cell[0]][next_cell[1]] = EMPTY
+    grid[front[0]][front[1]] = BOX
+    return next_cell
 
 
 def play_wide(grid_str, moves):
@@ -64,67 +69,11 @@ def play_wide(grid_str, moves):
 
     for move in moves:
         curr_dir = DIRS[move]
-        next_cell = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
-
-        if grid[next_cell[0]][next_cell[1]] == EMPTY:
-            curr = next_cell
-            continue
-
-        if grid[next_cell[0]][next_cell[1]] == WALL:
-            continue
 
         if curr_dir[0] == 0:
-            front = next_cell
-
-            while grid[front[0]][front[1]] in WIDE_BOX:
-                front = (front[0] + curr_dir[0], front[1] + curr_dir[1])
-
-            if grid[front[0]][front[1]] != EMPTY:
-                continue
-
-            while front != next_cell:
-                second = (front[0] - curr_dir[0], front[1] - curr_dir[1])
-                grid[front[0]][front[1]] = grid[second[0]][second[1]]
-                front = second
-
-            grid[next_cell[0]][next_cell[1]] = EMPTY
-            curr = next_cell
+            curr = move_wide_h(grid, curr, curr_dir)
         else:
-            front = {curr}
-            movable = set()
-            should_move = None
-            while should_move is None:
-                new_front = set()
-                for cell in list(front):
-                    next_front_cell = (cell[0] + curr_dir[0], cell[1] + curr_dir[1])
-
-                    if grid[next_front_cell[0]][next_front_cell[1]] == WALL:
-                        should_move = False
-                        break
-
-                    if grid[next_front_cell[0]][next_front_cell[1]] == EMPTY:
-                        continue
-
-                    new_front.add(next_front_cell)
-                    movable.add(next_front_cell)
-
-                    if grid[next_front_cell[0]][next_front_cell[1]] == WIDE_BOX[0]:
-                        new_front.add((next_front_cell[0], next_front_cell[1] + 1))
-                        movable.add((next_front_cell[0], next_front_cell[1] + 1))
-                    else:
-                        new_front.add((next_front_cell[0], next_front_cell[1] - 1))
-                        movable.add((next_front_cell[0], next_front_cell[1] - 1))
-
-                front = new_front
-                if should_move is None and len(front) == 0:
-                    should_move = True
-
-            if should_move:
-                move_list = sorted(list(movable), key=lambda x: x[0], reverse=curr_dir[0] == 1)
-                for cell in move_list:
-                    grid[cell[0] + curr_dir[0]][cell[1] + curr_dir[1]] = grid[cell[0]][cell[1]]
-                    grid[cell[0]][cell[1]] = EMPTY
-                curr = next_cell
+            curr = move_wide_v(grid, curr, curr_dir)
 
     total = 0
     for i in range(len(grid)):
@@ -133,6 +82,59 @@ def play_wide(grid_str, moves):
                 total += 100 * i + j
 
     return total
+
+
+def move_wide_h(grid, curr, curr_dir):
+    front = (curr[0] + curr_dir[0], curr[1] + curr_dir[1])
+
+    while grid[front[0]][front[1]] in WIDE_BOX:
+        front = (front[0] + curr_dir[0], front[1] + curr_dir[1])
+
+    if grid[front[0]][front[1]] != EMPTY:
+        return curr
+
+    while True:
+        second = (front[0] - curr_dir[0], front[1] - curr_dir[1])
+        grid[front[0]][front[1]] = grid[second[0]][second[1]]
+
+        if second == curr:
+            return front
+
+        front = second
+
+
+def move_wide_v(grid, curr, curr_dir):
+    front = {curr}
+    movable = set()
+    while len(front) > 0:
+        new_front = set()
+        for cell in list(front):
+            next_front_cell = (cell[0] + curr_dir[0], cell[1] + curr_dir[1])
+
+            if grid[next_front_cell[0]][next_front_cell[1]] == WALL:
+                return curr
+
+            if grid[next_front_cell[0]][next_front_cell[1]] == EMPTY:
+                continue
+
+            new_front.add(next_front_cell)
+            movable.add(next_front_cell)
+
+            if grid[next_front_cell[0]][next_front_cell[1]] == WIDE_BOX[0]:
+                new_front.add((next_front_cell[0], next_front_cell[1] + 1))
+                movable.add((next_front_cell[0], next_front_cell[1] + 1))
+            else:
+                new_front.add((next_front_cell[0], next_front_cell[1] - 1))
+                movable.add((next_front_cell[0], next_front_cell[1] - 1))
+
+        front = new_front
+
+    move_list = sorted(list(movable), key=lambda x: x[0], reverse=curr_dir[0] == 1)
+    for cell in move_list:
+        grid[cell[0] + curr_dir[0]][cell[1] + curr_dir[1]] = grid[cell[0]][cell[1]]
+        grid[cell[0]][cell[1]] = EMPTY
+
+    return curr[0] + curr_dir[0], curr[1] + curr_dir[1]
 
 
 def main():
