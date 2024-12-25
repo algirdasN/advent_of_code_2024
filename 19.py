0bbf6cc09d7a2f5462f95235698c6a6aede6333e
Algirdas Neniskis <algirdas.neniskis@gmail.com>
2024-12-25 05:51:37 +0200

    Problem 19 - extracting method

--- 19.py
+++ 19.py
@@ -10,20 +10,38 @@ DIRS = [
 WALL = "#"
 
 
+def calculate_cheats(grid_map, path, len_x, len_y, cheat_length, save_cutoff):
+    total = 0
+    for jump_from in path:
+        for d in DIRS:
+            skip_cell = jump_from[0] + d[0], jump_from[1] + d[1]
+            if grid_map[skip_cell] != WALL:
+                continue
+
+            jump_to = jump_from[0] + 2 * d[0], jump_from[1] + 2 * d[1]
+            if 0 <= jump_to[0] < len_x and 0 <= jump_to[1] < len_y \
+                    and grid_map[jump_to] != WALL \
+                    and path[jump_to] - path[jump_from] - cheat_length >= save_cutoff:
+                total += 1
+
+    return total
+
+
 def main():
     with open("data/19.txt") as file:
         grid = file.read().splitlines()
 
+    len_x = len(grid)
+    len_y = len(grid[0])
     grid_map = {(i, j): value for i, sublist in enumerate(grid) for j, value in enumerate(sublist)}
 
     start = (-1, -1)
     end = (-1, -1)
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == "S":
-                start = (i, j)
-            if grid[i][j] == "E":
-                end = (i, j)
+    for k, v in grid_map.items():
+        if v == "S":
+            start = k
+        if v == "E":
+            end = k
 
     curr = start
     path = {}
@@ -40,20 +58,7 @@ def main():
                 curr = next_cell
                 break
 
-    total = 0
-    for jump_from in path:
-        for d in DIRS:
-            skip_cell = jump_from[0] + d[0], jump_from[1] + d[1]
-            if grid_map[skip_cell] != WALL:
-                continue
-
-            jump_to = jump_from[0] + 2 * d[0], jump_from[1] + 2 * d[1]
-            if 0 <= jump_to[0] < len(grid) and 0 <= jump_to[1] < len(grid[0]) \
-                    and grid_map[jump_to] != WALL \
-                    and path[jump_to] - path[jump_from] - 2 >= 100:
-                total += 1
-
-    print(total)
+    print(calculate_cheats(grid_map, path, len_x, len_y, 2, 100))
 
 
 if __name__ == "__main__":
